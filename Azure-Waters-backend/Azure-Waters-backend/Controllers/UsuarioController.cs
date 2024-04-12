62e1d9785500e9e122498e0f4ede09ff3888126f
83gerson <gersoncalvo83@gmail.com>
2024-04-12 01:13:56 -0600

    Backend funcional
    Se realizó el buscarUsuario por nombre de usuario y contraseña

--- Azure-Waters-backend/Azure-Waters-backend/Controllers/UsuarioController.cs
+++ Azure-Waters-backend/Azure-Waters-backend/Controllers/UsuarioController.cs
@@ -23,6 +23,36 @@ namespace Azure_Waters_backend.Controllers
 
             return Ok(usuarios);
         }
+
+        [HttpPost]
+        [Route(nameof(buscarUsuario))]
+        public async Task<Usuario> buscarUsuario(string usuario, string contrasenna)
+        {
+            Usuario login = new Usuario();
+            var buscarLogin = new Usuario();
+            using (var _context = new AzureWatersContext())
+            {
+                buscarLogin = await (from ua in _context.Usuarios
+                                     where ua.NombreUsuario == usuario && ua.Contrasenna == contrasenna
+                                     select new Usuario
+                                     {
+                                         UsuarioId = ua.UsuarioId,
+                                         NombreUsuario = ua.NombreUsuario,
+                                         Contrasenna = ua.Contrasenna
+                                     }).FirstOrDefaultAsync();
+            }
+
+            if (buscarLogin == null)
+            {
+                login.UsuarioId = null;
+                login.NombreUsuario = null;
+            }
+            else
+            {
+                login = buscarLogin;
+            }
+            return login;
+        }
     }
 
 }
